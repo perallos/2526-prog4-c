49e0650d693a9f337a750f29e4b0e186f5ff079b
perallos <perallos@gmail.com>
2026-02-11 10:18:04 +0100

    Subida Ej03a

---
+++ Pr_C.Ej03a_Control/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void echo(void);
+
+int main(void)
+{
+    echo();
+}
+
+void echo(void)
+{ 
+    char c;
+
+    do
+    {
+        c = getchar();
+        if (c != '\n')
+        {
+            c -= 'a' - 'A';
+            printf("Has introducido la letra: %c. ASCII = %i\n", c, c);
+            fflush(stdout);
+        }
+    } while (c != 'Q');
+}
